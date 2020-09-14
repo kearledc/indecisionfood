0e6709aaa1d81216de566f690b758960146ba8a1
Kevin Earle Cruz <k.earle.dc@gmail.com>
2020-09-14 18:47:33 +0800

    Updates

---
+++ precache-manifest.13714bcad592aede0b432a23474256b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a916455a5e619d2210d40615b22b4a5",
+    "url": "/indecisionfood/index.html"
+  },
+  {
+    "revision": "0f463d262ce7dd2d6629",
+    "url": "/indecisionfood/static/css/main.0a503839.chunk.css"
+  },
+  {
+    "revision": "6cad07ca5985dc778bfb",
+    "url": "/indecisionfood/static/js/2.430cd373.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/indecisionfood/static/js/2.430cd373.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f463d262ce7dd2d6629",
+    "url": "/indecisionfood/static/js/main.c9433a22.chunk.js"
+  },
+  {
+    "revision": "2313a50810d26d1139e5",
+    "url": "/indecisionfood/static/js/runtime-main.99b68217.js"
+  }
+]);
